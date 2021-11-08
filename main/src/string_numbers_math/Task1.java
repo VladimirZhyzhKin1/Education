168ff0373c566dbc0494e2f834e4f25ae5529202
Vladimir Zhyzhkin <v.zhyzhkin@biclast.com>
2021-11-08 17:25:37 +0300

    "Task 1 string, number, math"

--- main/src/string_numbers_math/Task1.java
+++ main/src/string_numbers_math/Task1.java
@@ -1,6 +1,9 @@
+package string_numbers_math;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 //Подсчет повторяющихся символов. Написать программу, которая подсчитывает повторяющиеся символы в заданной строке.
 
@@ -10,7 +13,9 @@ public class Task1 {
         Scanner in = new Scanner(System.in);
         System.out.println("Input text: ");
         String text = in.next();
-        System.out.println(countDublicateCharacters(text));
+        //System.out.println(countDublicateCharacters(text));
+        //System.out.println(countDublicateCharacters2(text));
+        System.out.println(countDublicateCharactersSurrogateCouple(text));
     }
 
     public static Map<Character, Integer> countDublicateCharacters(String text) {
@@ -21,4 +26,20 @@ public class Task1 {
         }
         return result;
     }
+
+    public static Map<Character, Long> countDublicateCharacters2(String text) {
+        Map<Character, Long> result = text.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        return result;
+    }
+
+    //с учетом суррогатных пар Unicode
+    public static Map<String, Long> countDublicateCharactersSurrogateCouple(String text) {
+        Map<String, Long> result = text.codePoints()
+                .mapToObj(c -> String.valueOf(Character.toChars(c)))
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        return result;
+    }
+
 }
